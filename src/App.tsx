885336a1c41c3e7355bc284e66f3a862be583da6
g.remigi <g.remigi@kineticsystem.org>
2022-03-31 16:05:19 +0200

    Code simplification.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import PulsatingBox from "./components/PulsatingBox";
 import Marker from "./components/Marker";
 
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 
 // React-Three fiber.
 import { Canvas } from "@react-three/fiber";
@@ -9,12 +9,8 @@ import { Canvas } from "@react-three/fiber";
 // React-Three fiber components.
 import { OrbitControls } from "@react-three/drei";
 
-// We need this library to correctly use refs with Typescript.
-import { OrbitControls as OrbitControlsImpl } from "three-stdlib";
-
 function App() {
   const [orbitEnabled, setOrbitEnabled] = useState(true);
-  const orbit = useRef<OrbitControlsImpl>(null!);
 
   return (
     <Canvas camera={{ fov: 45, position: [-4, 4, -7] }} shadows>
@@ -25,7 +21,7 @@ function App() {
         position={[-2, 0.6, -2]}
       />
       <gridHelper args={[4, 10, 0x3a3a3a, 0x3a3a3a]} />
-      <OrbitControls ref={orbit} enabled={orbitEnabled} dampingFactor={1} />
+      <OrbitControls enabled={orbitEnabled} dampingFactor={1} />
       <Marker
         minRingRadius={1.0}
         ringSize={0.5}
